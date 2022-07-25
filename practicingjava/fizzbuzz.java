0f36e269fa983d358daa6724bd91593f60c1a8b2
Mahshad <mahshad.dezhsetan@gmail.com>
2022-07-25 14:14:42 +0400

    delete .idea

---
+++ practicingjava/fizzbuzz.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class fizzbuzz {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("number:  ");
+        int number = scanner.nextInt();
+        System.out.println(number);
+    }
+}
